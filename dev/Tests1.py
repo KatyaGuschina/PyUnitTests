e68a9efedbe5da577e77e70838001d66b917766c
KatyaGuschina <katya.guschina@gmail.com>
2015-02-22 23:21:27 +0600

    initial commit

---
+++ dev/Tests1.py
@@ -0,0 +1,18 @@
+'''
+Created on Feb 23, 2015
+
+@author: kguschina
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def test1(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test1']
+    unittest.main()
