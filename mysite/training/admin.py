428a54d7a317431ef0414931e5b51f44b02d4e8a
KatyaGuschina <katya.guschina@gmail.com>
2015-02-24 16:50:04 +0600

    delete proj

--- mysite/training/admin.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-from django.contrib import admin
-from training.models import Task
-
-class TaskionAdmin(admin.ModelAdmin):
-    # ...
-    list_display = ('content', 'created', 'was_published_recently')
--- mysite/training/urls.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from django.conf.urls import patterns, url
-
-from training.views import TaskDetailView, TaskListView
-
-urlpatterns = patterns('',
-    url(r'^$', TaskListView.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)/$', TaskDetailView.as_view()),
-)
